0a6ee246ab4d70a79374e545dc1072e31cee5f8a
SungWoo <sungwoo166@gmail.com>
2023-10-22 13:37:12 +0900

    .

--- src/삼성SW_EXPERT_ACADEMY/D3/암호문1/Solution.java
+++ src/삼성SW_EXPERT_ACADEMY/D3/암호문1/Solution.java
@@ -3,7 +3,6 @@ package 삼성SW_EXPERT_ACADEMY.D3.암호문1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -20,30 +19,22 @@ public class Solution {
             bf.readLine();
             st = new StringTokenizer(bf.readLine());
             while (st.hasMoreTokens()) {
-                st.nextToken();
                 String command = st.nextToken();
-                if (command == "I") {
-                    System.out.println(st.nextToken());
-                    System.out.println(st.nextToken());
-
-//                    int index = Integer.parseInt(st.nextToken());
-//                    int length = Integer.parseInt(st.nextToken());
-//                    System.out.println(index);
-//                    System.out.println(length);
-//                    ArrayList<String> newList = new ArrayList<>();
-//                    for (int i = 0; i < length; i++) {
-//                        newList.add(st.nextToken());
-//                    }
-//                    arrayList.addAll(index, newList);
+                if (command.equals("I")) {
+                    int index = Integer.parseInt(st.nextToken());
+                    int length = Integer.parseInt(st.nextToken());
+                    ArrayList<String> newList = new ArrayList<>();
+                    for (int i = 0; i < length; i++) {
+                        newList.add(st.nextToken());
+                    }
+                    arrayList.addAll(index, newList);
                 }
             }
-//            System.out.print("#" + T + " ");
-//            for (int i = 0; i < arrayList.size(); i++) {
-//                System.out.print(arrayList.get(i) + " ");
-//            }
-//            System.out.println("");
-
+            System.out.print("#" + T + " ");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(arrayList.get(i) + " ");
+            }
+            System.out.println("");
         }
-
     }
 }
---
+++ src/삼성SW_EXPERT_ACADEMY/D3/회문2/Solution.java
@@ -0,0 +1,4 @@
+package 삼성SW_EXPERT_ACADEMY.D3.회문2;
+
+public class Solution {
+}
