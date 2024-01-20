c0be06a7d9a7ad425ec4ff798eb7cc2256767a46
hennible0612 <sungwoo166@gmail.com>
2024-01-20 22:39:19 +0900

    1927

---
+++ src/A형/백준/_1927.java
@@ -0,0 +1,28 @@
+package A형.백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class _1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int userInput = Integer.parseInt(bf.readLine());
+            if (userInput != 0) {
+                minHeap.add(userInput);
+            } else {
+                if (minHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(minHeap.poll());
+                }
+            }
+        }
+    }
+}
