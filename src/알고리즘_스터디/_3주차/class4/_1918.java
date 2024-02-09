a1c13062334f5f25862e9cf3eda597f718888fd4
SungWoo <sungwoo166@gmail.com>
2024-02-09 20:13:08 +0900

    1918

--- src/알고리즘_스터디/_3주차/class4/_1918.java
+++ src/알고리즘_스터디/_3주차/class4/_1918.java
@@ -1,7 +1,56 @@
 package 알고리즘_스터디._3주차.class4;
 
-public class _1918 {
-    public static void main(String[] args) throws Exception {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class _1918 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String infix = bf.readLine();
+        StringBuilder postfix = new StringBuilder(); 
+        Deque<Character> stack = new ArrayDeque<>(); 
+
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+
+            if (Character.isLetter(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop();
+            } else { 
+                while (!stack.isEmpty() && checker(c) <= checker(stack.peek())) {
+                    postfix.append(stack.pop()); 
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        System.out.println(postfix.toString()); 
+    }
+
+    public static int checker(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return -1;
+        }
     }
 }
