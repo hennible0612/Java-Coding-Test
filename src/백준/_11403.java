c6512cb3ed0427994d4b67566ce8ddfd20b9ad66
hennible0612 <sungwoo166@gmail.com>
2024-03-07 21:03:50 +0900

    11403

--- src/백준/_11403.java
+++ src/백준/_11403.java
@@ -1,10 +1,51 @@
 package 백준;
 
-public class _11403 {
-    public static void main(String[] args) throws Exception{
-        System.out.println("hello world");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class _11403 {
+    static int N;
+
+    static int[][] distanceMap;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        N = Integer.parseInt(bf.readLine());
+        StringTokenizer st;
+        distanceMap = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < N; j++) {
+                distanceMap[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        solution();
     }
 
+    private static void solution() {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (distanceMap[i][k] == 1 && distanceMap[k][j] == 1) {
+                        distanceMap[i][j] = 1;
+                    }
+                }
+            }
+        }
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(distanceMap[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
